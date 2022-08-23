ca7ade21b6599b22867f8850061576c2904e8953
mahendrasaikumargandham <mahendragandham730@gmail.com>
2022-08-23 09:56:29 +0530

    Added Recent code

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app)
-const io = require("socket.io")(server)
+const cors = require("cors");
+const server = require("http").createServer(app)
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
+app.use(cors());
 
-const users = [];
+let users = [];
+let host;
 
-const addUser = (userName, roomId) => {
+const addUser = (userName, userEmail, roomId, currentLocation, reachedTime) => {
     users.push({
         userName: userName,
-        roomId: roomId
+        userEmail: userEmail,
+        roomId: roomId,
+        currentLocation: currentLocation,
+        reachedTime: reachedTime
     })
 }
 
@@ -20,22 +31,57 @@ const getRoomUsers = (roomId) => {
     return users.filter(user => (user.roomId == roomId));
 }
 
-const port = 3001;
+const port = 3000;
 app.get("/", (req, res) => {
     res.send("Rubix Private Server");
 })
 
 io.on("connection", socket => {
     console.log("Someone Connected")
-    socket.on("join-room", ({ userName, roomId }) => {
+    socket.on("join-room", ({ userName, userEmail, roomId, currentLocation, reachedTime }) => {
         console.log("User joined room");
         console.log(userName)
         console.log(roomId)
-        socket.join(roomId) 
-        addUser(userName, roomId)
-        socket.to(roomId).emit("user-connected", userName);
-        io.to(roomId).emit("all-users", getRoomUsers(roomId));
+        console.log(userEmail)
+        console.log(currentLocation)
+        if(roomId && userName) {
+            socket.join(roomId) 
+            addUser(userName, userEmail, roomId, currentLocation, reachedTime)
+            socket.to(roomId).emit("user-connected", userName);
+            io.to(roomId).emit("all-users", getRoomUsers(roomId));
+            console.log(users);
+        }
 
+        socket.on("messages",({ userName, userEmail, message, currentLocation, reachedTime, messageType }) => {
+            console.log(userName)
+            console.log(message)
+            console.log(userEmail)
+            console.log(currentLocation)
+            console.log(reachedTime)
+            console.log(messageType)
+            io.emit("messages", ({ userName, userEmail, message, currentLocation, reachedTime, messageType }));
+        })
+
+        socket.on("callUser", ({ userToCall, signalData, from, name}) => {
+            io.to(userToCall).emit("callUser", {
+                signal: signalData,
+                from, 
+                name
+            })
+        })
+
+        socket.on("updateMyMedia", ({ type, currentMediaStatus }) => {
+            console.log("updateUserMedia")
+            socket.broadcast.emit("updateUserMedia", { type, currentMediaStatus })
+        })
+
+        socket.on("answerCall", (data) => {
+            socket.broadcast.emit("updateUserMedia", {
+                type: data.type,
+                currentMediaStatus: data.myMediaStatus,
+            })
+            io.to(data.to).emit("callAccepted", data);
+        })
         socket.on("disconnect", () => {
             console.log("Disconnected");
             socket.leave(roomId);
@@ -46,5 +92,5 @@ io.on("connection", socket => {
 })
 
 server.listen(port, ()=> {
-    console.log(`Rubix API Running on  3001`);
+    console.log(`Rubix API Running on  3000`);
 })
